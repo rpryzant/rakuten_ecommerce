e511c9db893196798541597ef3c7881fe115d07a
rapigan <rapigan@gmail.com>
2017-04-17 15:43:32 -0700

    example data, data processing

--- data_wrangling/tokenize_inputs.sh
+++ data_wrangling/tokenize_inputs.sh
@@ -5,10 +5,10 @@ CORPUS=$1
 spm_train \
   --input=${CORPUS} \
   --model_prefix=bpe \
-  --vocab_size=32000 \
+  --vocab_size=20000 \
   --model_type=bpe
 
 # apply BPE
 spm_encode --model=bpe.model --output_format=piece \
   < ${CORPUS} \
-  > ${CORPUS1}.bpe
+  > ${CORPUS}.bpe
