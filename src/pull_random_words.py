745f356db7222d825507f5bdb050a1e5ff8676b9
rpryzant <rapigan@gmail.com>
2017-04-21 14:47:38 -0700

    pull random

--- src/pull_random_words.py
+++ src/pull_random_words.py
@@ -7,16 +7,41 @@ python pull_random_words.py rnn_states-bahdanau-large/out.pkl \
                             ../data_wrangling/bpe.vocab \
                             health-random_baseline
 """
+from collections import defaultdict
+import sys
+import cPickle
+import numpy as np
+import random
+
+def build_vocab(vocab):
+    d = {}
+    for i, l in enumerate(vocab):
+        d[i + 2] = l.split()[0]      # +2 to reserve 0 for pad, 1 for unk
+    d[1] = 'UNK'
+    return d
+
+def get_pth(words, p):
+    cutoff = int(len(words) * (1 - p))
+    return words[:cutoff]
 
 
 
 
-# parse inference output
 
-# intersect with targets
+# load in args
+inference_output = sys.argv[1]
+labels = open(sys.argv[2])
+vocab = open(sys.argv[3])
+out_path = sys.argv[4]
 
-# put words in list
 
-# shuffle
 
-# return some of them
+vocab = build_vocab(vocab)
+words = vocab.values()
+random.shuffle(words)
+
+
+cutoff = int(len(words) * (1 - 0.8))
+with open(out_path, 'a') as out:
+    for i in range(cutoff):
+        out.write(words[i] + '\t' + '0.0' + '\n')
