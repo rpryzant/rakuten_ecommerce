33f63026bfa1d2492cc4e0aa5b4595ad9efb89cc
rpryzant <rapigan@gmail.com>
2017-04-21 17:00:20 -0700

    rm old config and arg parsing

--- src/inference.py
+++ src/inference.py
@@ -14,55 +14,10 @@ import argparse # option parsing
 
 
 
-def process_command_line():
-    """
-    Return a 1-tuple: (args list).
-    `argv` is a list of arguments, or `None` for ``sys.argv[1:]``.
-    """
-    parser = argparse.ArgumentParser(description='Usage') # add description
-    # positional arguments
-    parser.add_argument('checkpoint', metavar='checkpoint', type=str, help='model directory')
-    parser.add_argument('inputs', metavar='inputs', type=str, help='model inputs')
-    parser.add_argument('labels', metavar='labels', type=str, help='model outputs (labels)')
-    parser.add_argument('vocab', metavar='vocab', type=str, help='vocabulary')
-
-    # optional arguments
-    parser.add_argument('-t', '--attention-type', dest='attention_type', type=str, default='fc', 
-        help='what kind of attention to use: [bahdanau, dot, fc]')
-    parser.add_argument('-k', '--attention-keys', dest='attention_keys', type=str, default='rnn_states', 
-        help='what kind of attention keys to use: [rnn_states, word_vectors]')
-    parser.add_argument('-s', '--model-size', dest='model_size', type=str, default='medium', 
-        help='what size model: [small, medium, large]')
-    parser.add_argument('-g', '--gpu', dest='gpu', type=str, default='0', 
-        help='which gpu to run on')    
-    parser.add_argument('-o', '--output', dest='output', type=str, default=None, help='dump output to file')
- 
-    args = parser.parse_args()
-    return args
-
-
-
 def write_pickle(o, fname):
     with open(fname, 'w') as f:
         cPickle.dump(o, f, -1)
 
-
-
-
-def make_config(args):
-    print args
-    if args.model_size == 'small':
-        c = utils.SmallConfig()
-    elif args.model_size == 'medium':
-        c = utils.MediumConfig()
-    elif args.model_size == 'large':
-        c = utils.LargeConfig()
-
-    c.attention_type = args.attention_type
-    c.attention_keys = args.attention_keys
-
-
-    return c
 
 
 
--- src/trainer.py
+++ src/trainer.py
@@ -13,50 +13,6 @@ import argparse # option parsing
 
 
 
-def process_command_line():
-    """
-    Return a 1-tuple: (args list).
-    `argv` is a list of arguments, or `None` for ``sys.argv[1:]``.
-    """
-    parser = argparse.ArgumentParser(description='Usage') # add description
-    # positional arguments
-    parser.add_argument('checkpoint', metavar='checkpoint', type=str, help='model directory')
-    parser.add_argument('inputs', metavar='inputs', type=str, help='model inputs')
-    parser.add_argument('labels', metavar='labels', type=str, help='model outputs (labels)')
-    parser.add_argument('vocab', metavar='vocab', type=str, help='vocabulary')
-
-    # optional arguments
-    parser.add_argument('-t', '--attention-type', dest='attention_type', type=str, default=None, 
-        help='what kind of attention to use: [bahdanau, dot, fc]')
-    parser.add_argument('-k', '--attention-keys', dest='attention_keys', type=str, default=None, 
-        help='what kind of attention keys to use: [rnn_states, word_vectors]')
-    parser.add_argument('-s', '--model-size', dest='model_size', type=str, default='medium', 
-        help='what size model: [small, medium, large]')
-    parser.add_argument('-g', '--gpu', dest='gpu', type=str, default='0', 
-        help='which gpu to run on')
-
-
-#    parser.add_argument('-c', '--checkpoint', dest='checkpoint', type=str, default=None, help='model directory')
-    args = parser.parse_args()
-    return args
-
-
-
-def make_config(args):
-    if args.model_size == 'small':
-        c = utils.SmallConfig()
-    elif args.model_size == 'medium':
-        c = utils.MediumConfig()
-    elif args.model_size == 'large':
-        c = utils.LargeConfig()
-
-    if args.attention_type is not None:
-        c.attention_type = args.attention_type
-    if args.attention_keys is not None:
-        c.attention_keys = args.attention_keys
-
-    return c
-
 
 def main(args):
     if not os.path.exists(args.checkpoint):
