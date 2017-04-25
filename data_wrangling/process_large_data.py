5d1a3c04976e40aa735604a398df05cb8e26ff30
rpryzant <rapigan@gmail.com>
2017-04-25 05:58:09 -0700

    processing

---
+++ data_wrangling/process_large_data.py
@@ -0,0 +1,10 @@
+"""
+process a large data dump from john: 
+    files have one item per line,
+    but there are no sales units that I know of
+
+python process_large_data.py ../data/large/raw/health.test.out.tr
+
+"""
+
+
